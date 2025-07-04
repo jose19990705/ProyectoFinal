8fa9f95f43537a142c6eff6d3bf135c136cbddc8
Juan Carlos <juanc.medina1@udea.edu.co>
2025-07-03 21:07:40 -0500

    Botones de las opciones en fila, paginas de las opciones y boton de eventos

--- lib/pages/my_profile_page.dart
+++ lib/pages/my_profile_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:laboratorio_3/pages/repository/firebase_api.dart';
 import 'package:laboratorio_3/pages/sign_in_Page.dart';
-
+//comentario
 class MyProfilePage extends StatefulWidget {
   const MyProfilePage({super.key});
 
